8d1cbf3c404ee92550838c6faed24fc6b3daa28f
Tony <antony.branquinho@gmail.com>
2023-12-04 18:08:42 -0300

    Facil

---
+++ Pares consecutivos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pares_consecutivos {
+    class Program {
+        static void Main(string[] args) {
+
+            int numero, soma;
+            
+            Console.Write("Digite um numero inteiro: ");
+            numero = int.Parse(Console.ReadLine());
+
+            soma = 0;
+            while (numero != 0) {
+                if (numero % 2 == 0) {
+                    soma = (numero) + (numero + 2) + (numero + 4) + (numero + 6) + (numero + 8);
+                }
+                else if (numero % 2 != 0) {
+                    soma = (numero + 1)+(numero+3)+(numero+5)+(numero+7)+(numero+9);
+                }
+                Console.WriteLine("Soma = " + soma);
+                Console.Write("Digite um numero inteiro: ");
+                numero = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
